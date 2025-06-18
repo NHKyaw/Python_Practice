440c55e5ad0511bbe33bb348265ee0a98cf70065
Nyan Htet Kyaw <115153455+NHKyaw@users.noreply.github.com>
2025-06-18 11:54:11 +0700

    Update order_fruits.py

--- order_fruits.py
+++ order_fruits.py
@@ -1,14 +1,15 @@
 available_fruits=["banana","apple","orange"]
 print (f"We serve",",".join(available_fruits))
 user_order= input("Enter your fruit: ").strip().lower()
+
 bucket=[]
 bucket.append(user_order)
 for i in available_fruits:
     if user_order in available_fruits:
-        addon = input("Do you want to add another fruit? (y/n): ")
+        addon = input("Do you want to add another fruit? (y/n): ").strip().lower()
         if addon == "y":
             while addon == "y":
-                addon_fruit = input("Enter Addon fruit name: ")
+                addon_fruit = input("Enter Addon fruit name: ").strip().lower()
                 if addon_fruit in available_fruits:
                     bucket.append(addon_fruit)
                     print (f"Your fruit {bucket} is being packaging")
